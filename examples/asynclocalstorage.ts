0472b035bddd84e43e5eceb2ad5d9d0006a734a9
Abhijeet Prasad <aprasad@sentry.io>
2024-06-24 23:00:46 -0400

    add experiments with api design

---
+++ examples/asynclocalstorage.ts
@@ -0,0 +1,28 @@
+let AsyncLocalStorageCache: typeof import('node:async_hooks').AsyncLocalStorage | undefined;
+
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `npm run dev` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `npm run deploy` to publish your worker
+ *
+ * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
+ * `Env` object can be regenerated with `npm run cf-typegen`.
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+export default {
+	async fetch(request, env, ctx): Promise<Response> {
+		if (!AsyncLocalStorageCache) {
+			try {
+				const asyncHooksImport = 'node:async_hooks';
+				AsyncLocalStorageCache = await import(asyncHooksImport).then((m) => m.AsyncLocalStorage);
+			} catch (e) {
+				// console.error('Failed to import AsyncLocalStorage', e);
+			}
+			console.log(AsyncLocalStorageCache);
+		}
+		return new Response('Hello World!');
+	},
+} satisfies ExportedHandler<Env>;
